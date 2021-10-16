08daf6471417671871cf83bde89012460720461e
Akshat Kakkad <akshat.kakkad2802@gmail.com>
2021-10-16 11:56:34 +0530

    Extracted data from excel Sheet to Webpage

--- QuestionAdd_Manual/add_question.php
+++ QuestionAdd_Manual/add_question.php
@@ -10,6 +10,7 @@
 <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
 <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <link rel="stylesheet" href="style.css">
+<script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
 <!-- <link rel="stylesheet" href="style2.css"> -->
 <script src="myFile.js"></script>
 </head>
@@ -18,7 +19,10 @@
     <div class="modal-dialog">
       <div class="modal-content">
          <div class="modal-header">
-            <h3><span class="label label-warning" id="qid">Question</span><input type="text"></h3>
+            <!-- <h5><span class="label label-warning" id="qid">Question</span><textarea id="w3review" name="w3review" rows="4" cols="25">
+</textarea></h5> -->
+            <h3><span class="label label-warning" id="qid">Question</span><input type="textarea"></h3>
+            
         </div>
         <div class="modal-body">
             <div class="col-xs-3 col-xs-offset-5">
@@ -35,11 +39,11 @@
           </div>
 
           <div class="quiz" id="quiz" data-toggle="buttons">
-           <label class="element-animation1 btn btn-lg btn-primary btn-block"><span class="btn-label"><i class="glyphicon glyphicon-chevron-right"></i></span> <input type="text" name="q_answer" value="1" ></label>
-           <label class="element-animation2 btn btn-lg btn-primary btn-block"><span class="btn-label"><i class="glyphicon glyphicon-chevron-right"></i></span> <input type="text" name="q_answer" value="2"></label>
-           <label class="element-animation3 btn btn-lg btn-primary btn-block"><span class="btn-label"><i class="glyphicon glyphicon-chevron-right"></i></span> <input type="text" name="q_answer" value="3"></label>
-           <label class="element-animation4 btn btn-lg btn-primary btn-block"><span class="btn-label"><i class="glyphicon glyphicon-chevron-right"></i></span> <input type="text" name="q_answer" value="4"></label>
-           <label class="element-animation4 btn btn-lg btn-primary btn-block"><span class="btn-label"><i class="glyphicon glyphicon-chevron-right"></i></span> <input type="text" name="q_answer" value="5"></label>
+           <label class="element-animation1 btn btn-lg btn-primary btn-block"><span style = "padding-top: 20px;" class="btn-label"><b>A</b></span> <input type="text" name="q_answer" value="1" ></label>
+           <label class="element-animation2 btn btn-lg btn-primary btn-block"><span style = "padding-top: 20px;" class="btn-label"><b>B</b></span> <input type="text" name="q_answer" value="2"></label>
+           <label class="element-animation3 btn btn-lg btn-primary btn-block"><span style = "padding-top: 20px;" class="btn-label"><b>C</b></span> <input type="text" name="q_answer" value="3"></label>
+           <label class="element-animation4 btn btn-lg btn-primary btn-block"><span style = "padding-top: 20px;" class="btn-label"><b>D</b></span> <input type="text" name="q_answer" value="4"></label>
+           <label class="element-animation4 btn btn-lg btn-primary btn-block"><span style = "padding-top: 20px;" class="btn-label"><i class="fas fa-check-circle"></i></span> <input type="text" name="q_answer" value="5"></label>
        </div>
    </div>
    <div class="modal-footer text-muted">
